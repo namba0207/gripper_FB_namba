ac34d3ee5a6aa0776ce3e9483e5d4919c3ccaa05
zuma <cma12006@ict.nitech.ac.jp>
2024-01-22 14:57:23 +0900

    fix

--- Experiment1/Python/gripper_outflag0118.py
+++ Experiment1/Python/gripper_outflag0118.py
@@ -1,13 +1,13 @@
 # Arduino_gripper_out.inoとセット！！,押し込み表現拡大，ロボtグリッパー掴みから100まで
-import csv
+# import csv
 import math
 import threading
 import time
-import queue
 from pynput import mouse, keyboard
-import sys
-
-import numpy as np
+# import sys
+import random
+from random import randint
+# import numpy as np
 import serial
 from ArmWrapper1000 import ArmWrapper
 from xarm.wrapper import XArmAPI
@@ -15,6 +15,9 @@ from xarm.wrapper import XArmAPI
 
 class Text_class:
     def __init__(self):
+        self.oshikomi = []
+        self.speed = []
+        self.sample_list = [1, 2, 4]
         self.data2 = 400
         self.num = 0
         self.grippos = 0
@@ -37,22 +40,48 @@ class Text_class:
         self.arm.set_mode(0)
         self.arm.set_state(0)
 
-        thr2 = threading.Thread(target=self.sendloop)
-        thr2.setDaemon(True)
-        thr2.start()
-        thr3 = threading.Thread(target=self.moveloop)
-        thr3.setDaemon(True)
-        thr3.start()
+        thr0 = threading.Thread(target=self.sendloop)
+        thr0.setDaemon(True)
+        thr0.start()
         
-    def press(key):
+    def press(self,key):
         try:
             print('アルファベット {0} が押されました'.format(key.char))
+            if format(key.char) == '0':
+                self.num1 = randint(1, 2)
+                self.num2 = randint(1, 2)
+                self.num3 = randint(1, 2)
+                self.numlist = random.sample(self.sample_list, 3)
+                print(self.num1,self.num2,self.num3,self.numlist)
+                j = 0
+                
+                while j < 3:
+                    if self.numlist[j] == 1:
+                        self.oshikomi += [random.choice((160, 220))]
+                    if self.numlist[j] == 2:
+                        self.oshikomi += [random.choice((180, 230))]
+                    if self.numlist[j] == 4:
+                        self.oshikomi += [random.choice((200, 240))]
+                    j += 1
+                self.speed = [
+                    random.choice((0.5, 1, 2)),
+                    random.choice((0.5, 1, 2)),
+                    random.choice((0.5, 1, 2)),
+                ]
+                k = 0
+                while k < 3:
+                    print("self.oshikomi" + str(k) + ", self.speed"+ str(k) +" = "+ str(self.oshikomi[k])+ ","+ str(self.speed[k]))
+                    k += 1
+                
             if format(key.char) == '1':
-                print(1)
+                thr1 = threading.Thread(target=self.moveloop)
+                thr1.setDaemon(True)
+                thr1.start()
+            
         except AttributeError:
             print('スペシャルキー {0} が押されました'.format(key))
 
-    def release(key):
+    def release(self,key):
         if key == keyboard.Key.esc:     # escが押された場合
             return False    # listenerを止める
         
@@ -84,39 +113,46 @@ class Text_class:
             time.sleep(0.01)
 
     def moveloop(self):
-        self.start_time = time.perf_counter()
-        while True:
-            self.data1 = time.perf_counter() - self.start_time
-            if self.data1 < 2:
-                self.data2 = 400 - (400 - self.oshikomi) / (
-                    1 + self.e ** -(self.data1 * self.speed * 20 - 10)
+        i = 0
+        while i<3:
+            self.start_time = time.perf_counter()
+            while self.data1 < 5:
+                self.data1 = time.perf_counter() - self.start_time
+                if self.data1 < 2:
+                    self.data2 = 400 - (400 - self.oshikomi[i]) / (
+                        1 + self.e ** -(self.data1 * self.speed[i] * 20 - 10)
+                    )
+                elif self.data1 < 4:
+                    self.data2 = 400 - (400 - self.oshikomi[i]) / (
+                        1 + self.e ** (self.data1 * self.speed[i] * 20 - 30)
+                    )
+                else:
+                    self.data2 = 400
+                code, ret = self.arm.getset_tgpio_modbus_data(
+                    self.datal.ConvertToModbusData(self.data2)
                 )
-            elif self.data1 < 4:
-                self.data2 = 400 - (400 - self.oshikomi) / (
-                    1 + self.e ** (self.data1 * self.speed * 20 - 30)
-                )
-            code, ret = self.arm.getset_tgpio_modbus_data(
-                self.datal.ConvertToModbusData(self.data2)
-            )
-            time.sleep(0.005)
-
-
+                time.sleep(0.005)
+            i+=1
+            print(i)
+        print("mooveloop_finish")
+    
 if __name__ == "__main__":
     text_class = Text_class()
     listener = keyboard.Listener(
         on_press=text_class.press,
         on_release=text_class.release)
     listener.start()
-    
+    time.sleep(2)
     while True:
         try:
-            text_class.line = text_class.ser.readline().decode("utf-8").rstrip()
-            print(
-                # 2200 - int(self.arm.get_gripper_position()[1] / 400 * 2200),
-                text_class.datal.loadcell_int,
-                int(text_class.arm.get_gripper_position()[1]),
-                text_class.line,
-            )
+            pass
+            # text_class.line = text_class.ser.readline().decode("utf-8").rstrip()
+            # print(
+            #     # 2200 - int(self.arm.get_gripper_position()[1] / 400 * 2200),
+            #     text_class.datal.loadcell_int,
+            #     int(text_class.arm.get_gripper_position()[1]),
+            #     text_class.line,
+            # )
             # time.sleep(0.01)
         except KeyboardInterrupt:
             print("KeyboardInterrupt Stop:text")
--- Experiment1/Python/random_data.py
+++ Experiment1/Python/random_data.py
@@ -7,50 +7,18 @@ sample_list_pra = [1, 5]
 combined_list = []
 combined_new = []
 combined_pre = []
-i = 0
-while i < 3:
-    num = random.sample(sample_list_pra, 2)
-    print(num)
-    j = 0
-    oshikomi = []
-    speed = []
-    while j < 2:
-        if num[j] == 1:
-            oshikomi += [random.choice((160, 220))]
-        if num[j] == 5:
-            oshikomi += [random.choice((210, 250))]
-        j += 1
-    speed = [
-        random.choice((0.5, 1, 2)),
-        random.choice((0.5, 1, 2)),
-        random.choice((0.5, 1, 2)),
-    ]
-    k = 0
-    while k < 2:
-        print("self.oshikomi, self.speed = ", oshikomi[k], ",", speed[k])
-        k += 1
-    i += 1
-
 # i = 0
-# while i < 9:
-#     print(i + 1)
-#     num1 = randint(1, 2)
-#     num2 = randint(1, 2)
-#     num3 = randint(1, 2)
-#     num = random.sample(sample_list, 3)
-#     combined_list_new = [[num1, num2, num3], num]
-#     combined_list = combined_list + combined_list_new
-#     print(combined_list_new)
+# while i < 3:
+#     num = random.sample(sample_list_pra, 2)
+#     print(num)
 #     j = 0
 #     oshikomi = []
 #     speed = []
-#     while j < 3:
+#     while j < 2:
 #         if num[j] == 1:
 #             oshikomi += [random.choice((160, 220))]
-#         if num[j] == 2:
-#             oshikomi += [random.choice((180, 230))]
-#         if num[j] == 4:
-#             oshikomi += [random.choice((200, 240))]
+#         if num[j] == 5:
+#             oshikomi += [random.choice((210, 250))]
 #         j += 1
 #     speed = [
 #         random.choice((0.5, 1, 2)),
@@ -58,11 +26,43 @@ while i < 3:
 #         random.choice((0.5, 1, 2)),
 #     ]
 #     k = 0
-#     while k < 3:
+#     while k < 2:
 #         print("self.oshikomi, self.speed = ", oshikomi[k], ",", speed[k])
 #         k += 1
 #     i += 1
-# i = 0
-# while i < 18:
-#     print(combined_list[i], combined_list[i + 1])
-#     i += 2
+
+i = 0
+while i < 9:
+    print(i + 1)
+    num1 = randint(1, 2)
+    num2 = randint(1, 2)
+    num3 = randint(1, 2)
+    num = random.sample(sample_list, 3)
+    combined_list_new = [[num1, num2, num3], num]
+    combined_list = combined_list + combined_list_new
+    print(combined_list_new)
+    j = 0
+    oshikomi = []
+    speed = []
+    while j < 3:
+        if num[j] == 1:
+            oshikomi += [random.choice((160, 220))]
+        if num[j] == 2:
+            oshikomi += [random.choice((180, 230))]
+        if num[j] == 4:
+            oshikomi += [random.choice((200, 240))]
+        j += 1
+    speed = [
+        random.choice((0.5, 1, 2)),
+        random.choice((0.5, 1, 2)),
+        random.choice((0.5, 1, 2)),
+    ]
+    k = 0
+    while k < 3:
+        print("self.oshikomi" + str(k) + ", self.speed"+ str(k) +" = "+ str(oshikomi[k])+ ","+ str(speed[k]))
+        k += 1
+    i += 1
+i = 0
+while i < 18:
+    print(combined_list[i], combined_list[i + 1])
+    i += 2
