ea2710295445f19a55292e4bc51f56d24ee3c846
zuma <cma12006@ict.nitech.ac.jp>
2024-01-29 20:17:16 +0900

    fix

---
+++ Experiment1/Arduino/M5stack_hardness.ino
@@ -0,0 +1,36 @@
+#include <M5Stack.h>
+#include <GoPlus2.h>
+#include <math.h>
+
+GoPlus2 goPlus;
+float sensorValue_init = 0;
+int val = 0;
+int angle = 0;
+
+void setup(){
+  Serial.begin(115200);
+  M5.begin();
+  goPlus.begin();
+  M5.Power.begin();
+  M5.Lcd.setTextSize(2);//画面表示の大きさ
+}
+void loop(){
+  if (Serial.available()) {
+    val = Serial.read();
+    }
+  if (val > 250){
+    val = 250;
+  }
+  else if (val < 5){
+    val = 5;
+  }
+  angle = map(val,5,250,0,180);
+  goPlus.Servo_write_angle(SERVO_NUM0,angle);
+  delay(30);
+  M5.Lcd.setCursor(0, 0);
+  {
+      M5.Lcd.setTextColor(WHITE, BLACK);
+      M5.Lcd.printf("  raw data |  %0.4d  |\n",val);
+      M5.Lcd.printf("   angle   |  %0.4d  |\n",angle);
+  }
+}
