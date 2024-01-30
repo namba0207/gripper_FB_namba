0d745f3629b2758e258fe7b6d620dd82feb2370c
zuma <cma12006@ict.nitech.ac.jp>
2024-01-30 17:00:26 +0900

    fix

--- Experiment1/Arduino/M5stack_hardness.ino
+++ Experiment1/Arduino/M5stack_hardness.ino
@@ -3,42 +3,56 @@
 #include <math.h>
 
 GoPlus2 goPlus;
-// int raw_data = 0;
-int val = 0;
-int angle = 0;
-// float a = 0.8;//lowpassfft
+volatile float sensorValue_init = 0;
+volatile int val_h = 0;
+volatile int val = 0;
+volatile int angle_h = 0;
+volatile int angle = 0;
 
-void setup()
-{
+void setup(){
   Serial.begin(115200);
   M5.begin();
   goPlus.begin();
   M5.Power.begin();
-  M5.Lcd.setTextSize(2); // 画面表示の大きさ
+  M5.Lcd.setTextSize(2);//画面表示の大きさ
+  xTaskCreatePinnedToCore(subProcess, "subProcess", 4096, NULL, 1, NULL, 0); // 並列処理
+  sensorValue_init = goPlus.hub1_a_read_value(HUB_READ_ANALOG);
 }
-void loop()
-{
-  if (Serial.available())
-  {
-    val = Serial.read();//data取得
+void subProcess(void *pvParameters)
+{ // 並列処理の関数
+  while (1)
+    {
+    if (Serial.available()) {
+        val_h = Serial.read();
+    }
+    if (val_h > 255){
+      val_h = 255;
+    }
+    else if (val_h < 0){
+      val_h = 0;
+    }
+    angle_h = map(val_h,0,255,0,180);
+    goPlus.Servo_write_angle(SERVO_NUM2,angle_h);
+    Serial.println(val_h);
+    delay(10);
+    }
+}
+void loop(){
+  val = goPlus.hub1_a_read_value(HUB_READ_ANALOG)-sensorValue_init;
+  if (val > 250){
+    val = 250;
   }
-  // val = a * val + (1 - a) * raw_data;//lowpassfft
-  if (val > 255)
-  {
-    val = 255;
+  else if (val < 5){
+    val = 5;
   }
-  else if (val < 0)
-  {
-    val = 0;
-  }
-  Serial.println(val);
-  angle = map(val, 0, 255, 0, 180);
-  goPlus.Servo_write_angle(SERVO_NUM0, angle);
+  angle = map(val,5,250,0,180);
+  goPlus.Servo_write_angle(SERVO_NUM0,angle);
   delay(30);
   M5.Lcd.setCursor(0, 0);
   {
-    M5.Lcd.setTextColor(WHITE, BLACK);
-    M5.Lcd.printf("  raw data |  %0.4d  |\n", val);
-    M5.Lcd.printf("   angle   |  %0.4d  |\n", angle);
+      M5.Lcd.setTextColor(WHITE, BLACK);
+      M5.Lcd.printf("  raw data |  %0.4d  |\n",val);
+      M5.Lcd.printf("   angle   |  %0.4d  |\n",angle);
   }
+  
 }
