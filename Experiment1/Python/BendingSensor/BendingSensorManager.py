173a3f01e1051282651dc5a0052a9786b671ac11
zuma <cma12006@ict.nitech.ac.jp>
2024-01-30 18:19:42 +0900

    fix

--- Experiment1/Python/BendingSensor/BendingSensorManager.py
+++ Experiment1/Python/BendingSensor/BendingSensorManager.py
@@ -12,6 +12,7 @@ import RobotArmController.Robotconfig as RC
 import RobotArmController.Robotconfig_pos as RP
 import RobotArmController.Robotconfig_vib as RV
 import serial
+from scipy import stats as st
 from xarm.wrapper import XArmAPI
 
 
@@ -23,10 +24,15 @@ class BendingSensorManager:
         self.bendingValue = 400
         self.bendingValue_sub = 0
         self.ser = serial.Serial(ip, port)
+        self.ser2 = serial.Serial("COM8", 115200)
         self.not_used = self.ser.readline()
+        self.not_used = self.ser2.readline()
         self.arm = XArmAPI("192.168.1.199")
 
-    def thread(self):
+    def sendloop(self):
+        slope = 0
+        self.x_data = np.array([])
+        self.x_data = np.array([])
         while True:
             # 掴み始め・離し始め
             if self.flag == 0 and RC.num_int >= 129:
@@ -36,28 +42,51 @@ class BendingSensorManager:
                 self.grip = 0
                 self.flag = 0
             if self.flag == 0:
-                self.pos2 = int(0)
+                self.x_list = np.array([])
+                self.y_list = np.array([])
+                # self.pos2 = int(0)
             else:
-                self.pos2 = int(
-                    (self.grip - self.arm.get_gripper_position()[1])
-                    * (255 - 0)
-                    / (self.grip - 200)  # 止まるところでグリッパー閉じ切る
-                )
-            if self.pos2 > 255:
-                self.pos2 = 255
-            elif self.pos2 < 0:
-                self.pos2 = 0
-            self.num_str = str(RC.num_int) + "," + str(self.pos2) + "\n"
+                # self.pos2 = int(
+                #     (self.grip - self.arm.get_gripper_position()[1])
+                #     * (255 - 0)
+                #     / (self.grip - 200)  # 止まるところでグリッパー閉じ切る
+                # )
+                self.x_list = np.append(
+                        self.x_list, [self.grippos - self.arm.get_gripper_position()[1]]
+                    )
+                self.y_list = np.append(self.y_list, [self.datal.loadcell_int - 129])
+                if len(self.x_list) >= 10:
+                    self.x_data = np.array([self.x_list])
+                    self.y_data = np.array([self.y_list])
+                    if np.std(self.x_data) == 0:
+                        self.x_data = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+                    slope, intercept, r_value, p_value, std_err = st.linregress(
+                        self.x_data[-1:-11:-1], self.y_data[-1:-11:-1]
+                    )
+                    print("傾き:{0}".format(slope))
+                    self.slope_h = int(1 / slope * (255 - 0) / (3 - 0))
+                    if self.slope_h > 100:
+                        self.slope_h = 100
+                    elif self.slope_h < 0:
+                        self.slope_h = 0
+            # if self.pos2 > 255:
+            #     self.pos2 = 255
+            # elif self.pos2 < 0:
+            #     self.pos2 = 0
+            
+            # self.num_str = str(RC.num_int) + "," + str(self.pos2) + "\n"
+            self.num_str = str(RC.num_int) +  "\n"
             self.ser.write(self.num_str.encode())
+            self.ser2.write(bytes([self.slope_h]))
             # self.ser.write(bytes([RC.num_int]))
-            time.sleep(0.0005)
+            time.sleep(0.005)
 
     def StartReceiving(self):
         """
         Receiving data from bending sensor and update self.bendingValue
         """
         try:
-            thr = threading.Thread(target=self.thread)
+            thr = threading.Thread(target=self.sendloop)
             thr.setDaemon(True)
             thr.start()
             self.pretime = time.perf_counter()
