8be402a23c3e54836277ff830dfdd1b9cce82df0
takumi nishimura <112153843+Haptics-Lab@users.noreply.github.com>
2024-02-06 14:06:11 +0900

    fix

--- Experiment1/Python/udp_received.py
+++ Experiment1/Python/udp_received.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QFont
@@ -33,6 +34,12 @@ class CenterDisplayApp(QWidget):
         font.setPointSize(400)  # フォントサイズを300に設定
         self.center_label.setFont(font)
 
+        self.sum_time = 0
+        self.sum_time_recode = 0
+        self.start_time_float = 0
+        self.flag = 0
+        self.next_time = 0
+        self.count = 1
     def process_udp_datagrams(self):
         while self.udp_socket.hasPendingDatagrams():
             datagram, host, port = self.udp_socket.readDatagram(
@@ -40,7 +47,51 @@ class CenterDisplayApp(QWidget):
             )
             data_list = datagram.data().decode().split(",")
             received_number = int(data_list[1])
-            self.center_label.setText(str(received_number))
+            print_str = str(received_number)+ " 0"
+            # クリアしていない場合
+            if (self.sum_time < 3 and self.flag !=3):
+                # flag = 1 から flag = 0のとき変換
+                if (received_number < 3000 and self.flag == 1):
+                    self.flag = 0
+                    print_str = str(received_number)+ " "+ str(int(self.sum_time))
+                    self.sum_time_recode = self.sum_time
+                # flag = 2 から flag = 0のときセンサ0のときflag = 0
+                elif received_number == 0:
+                    self.flag = 0
+                    print_str = str(received_number)+ " " + str(int(self.sum_time))
+                # flag = 0 から flag = 1のとき変換、タイマースタート
+                if (received_number >= 3000 and self.flag == 0):
+                    self.flag = 1
+                    self.start_time_float = time.perf_counter()
+                # flag = 1 から flag = 2のとき変換、やり直し出力、カウントリセット
+                if (received_number >= 3500 or self.flag == 2):
+                    self.flag = 2
+                    print_str = "OUT"
+                    self.sum_time = 0
+
+                # メインの処理
+                if self.flag == 1:
+                    self.sum_time = self.sum_time_recode + time.perf_counter() - self.start_time_float
+                    print_str = str(received_number) + " "  + str(int(self.sum_time))
+
+            # 3秒以上キープした場合
+            elif self.flag == 1:
+                self.sum_time = 0
+                self.flag = 3
+                self.next_time = time.perf_counter()
+
+            elif self.flag == 3:
+                print_str = "CLEAR" + str(self.count)
+
+            if (time.perf_counter() - self.next_time > 2 and self.count < 3 and self.flag == 3):
+                self.flag = 0
+                self.count += 1
+            elif (self.count == 3 and self.flag == 3):
+                print_str = "FINISH"
+
+            print(self.flag, self.next_time)
+            self.center_label.setText(print_str)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- Experiment1/Python/udp_send.py
+++ Experiment1/Python/udp_send.py
@@ -13,10 +13,6 @@ port = 8888
 
 ser = serial.Serial("COM8", 115200)
 not_used = ser.readline()
-flag = 0
-sum_time = 0
-count = 1
-
 # data_parts = line.split(",")
 # bendingValue_int = int(data_parts[0].rstrip())
 
@@ -24,31 +20,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
     while True:
         # 送る数値を一種類にする変数
         data_value = ser.readline().decode("utf-8").rstrip()
-        if sum_time < 300:
-            if int(data_value) >= 3000 and flag == 0:
-                flag = 1
-                start_time = time.perf_counter()*100
-            if int(data_value) >= 3000 and int(data_value) < 3500 and flag == 1:
-                sum_time = sum_time + time.perf_counter()*100 - start_time
-                data_value = data_value + str(int(sum_time))
-            if int(data_value) >= 3500 and flag == 1:
-                flag = 2
-                sum_time = 0
-                data_value = 'RETRY'
-            if int(data_value) < 3000 and flag == 1:
-                data_value = 'CONTINUE'
-            if int(data_value)  == 0:
-                flag = 0
-        else:
-            next_time = time.perf_counter()
-            flag = 3
-        if time.perf_counter() - next_time < 3 and flag == 3:
-            data_value = 'CLEAR' + str(count)
-        elif flag == 3:
-            sum_time = 0
-            flag = 0
-            count += 1
-        # print(data_value)
         # 左右どちらかを選択して送信
         data = f"right,{data_value}"  # または "left,{data_value}"
         message = data.encode("utf-8")
