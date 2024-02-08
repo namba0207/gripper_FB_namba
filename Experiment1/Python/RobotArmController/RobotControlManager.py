694cc5f6221696f1d4425818c22795f99bf96b91
takumi-nishimura <112153843+Haptics-Lab@users.noreply.github.com>
2024-02-08 17:15:39 +0900

    fix

--- Experiment1/Python/RobotArmController/RobotControlManager.py
+++ Experiment1/Python/RobotArmController/RobotControlManager.py
@@ -192,7 +192,7 @@ class RobotControlManager:
                     # print(localPosition["participant1"], "a")
 
                     with open(
-                        "xyzdata0208_nakadachkato13.csv", "a", newline=""
+                        "xyzdata0208_nakadachkato23.csv", "a", newline=""
                     ) as file:
                         writer = csv.writer(file)
                         writer.writerow(
