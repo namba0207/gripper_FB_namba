e8ca588fbe2ab2456caba1e6904ff51299bac076
namba0207 <cmj13129@ict.nitech.ac.jp>
2024-09-09 11:43:28 +0900

    fix

---
+++ Experiment1/Arduino/serial_souhoukou2/serial_souhoukou2.ino
@@ -0,0 +1,22 @@
+// // 2変数を2倍にして送信するプログラム
+// int A = 1;
+// int B = 1;
+// void setup()
+// {
+//   Serial.begin(115200);
+// }
+
+// void loop()
+// {
+//   if (Serial.available() > 0)
+//   {
+//     String data1 = Serial.readStringUntil('\n');
+//     String data2 = Serial.readStringUntil('\n');
+//     A = data1.toInt() * 2;
+//     B = data2.toInt() * 2;
+//   }
+//   Serial.print(A);
+//   Serial.print(',');
+//   Serial.println(B);
+// }
+const int 
--- Experiment1/Python/serial_souhoukou_success.py
+++ Experiment1/Python/serial_souhoukou_success.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env python3
+#2変数を送信(ser.write)・受信(readline)するプログラム
 import time
 
 import serial
 
 if __name__ == "__main__":
-    ser = serial.Serial("COM6", 115200, timeout=1)
+    ser = serial.Serial("/dev/cu.usbserial-2130", 115200, timeout=1)
     ser.flush()
-    # A = str(100) + "\n"
-    # B = str(333) + "\n"
+    A = str(100) + "\n"
+    B = str(333) + "\n"
     while True:
-        # ser.write(A.encode())
-        # ser.write(B.encode())
+        ser.write(A.encode())
+        ser.write(B.encode())
         line = ser.readline().decode("utf-8").rstrip()
         print(line)
         time.sleep(0.005)
