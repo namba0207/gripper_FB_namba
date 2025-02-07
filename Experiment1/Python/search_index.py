4822a81560fe40ba27befad972f639b80e14b496
namba0207 <cmj13129@ict.nitech.ac.jp>
2025-02-07 09:07:41 +0900

    fix

--- Experiment1/Python/search_index.py
+++
@@ -1,5 +0,0 @@
-import pyaudio
-
-iaudio = pyaudio.PyAudio()
-for i in range(0, iaudio.get_device_count()):
-    print(iaudio.get_device_info_by_index(i))
--- Experiment1/TouchDesigner/startup.bat
+++
@@ -1,4 +0,0 @@
-start "" "SpoutToNDI_2007/SpoutToNDI/bin/Spout to NDI.exe"
-start "" "SpoutToNDI_2007/SpoutToNDI/bin/Spout to NDI.exe"
-start "" "SpoutToNDI_2007/SpoutToNDI/bin/NDI to Spout.exe"
-start "" "SpoutToNDI_2007/SpoutToNDI/bin/NDI to Spout.exe"
